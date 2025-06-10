7f8b17ab6aa3633def000011177611fa43799c91
cs-abhinaw <abhinaw.kumar@91coderstreet.com>
2025-06-10 15:56:48 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Headers />
       <Routes>
         <Route path='/' element={<Home />} />
-        // <Route path='/login' element={<Login />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='*' element={<Error />} />
       </Routes>
